3aca4deeaf48aa8d57e8bc30e9da3f09163480ad
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-17 20:17:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ function congratulateCustomer(){
 
 function attemptTwoFavoriteCustomers(){
     let favoriteCustomer = 'tori'
-    let favoriteCustomer = 'not bar'
+    let favoriteCustomer = 'not tori'
 }
